ad12874cb11afa2cf49a350dd1e0a6a7a12e1824
RogerioLS <rogerio_288@hotmail.com>
2026-03-15 18:46:47 -0300

    🔧 chore(update): variable de object for obj

--- python_0_starting/ex02/find_ft_type.py
+++ python_0_starting/ex02/find_ft_type.py
@@ -1,17 +1,17 @@
-def all_thing_is_obj(object: any) -> int:
-    
-    obj_type = type(object)
+def all_thing_is_obj(obj: any) -> int:
+
+    obj_type = type(obj)
 
     if obj_type == list:
-        print("List :", obj_type)
+        print(f"List : {obj_type}")
     elif obj_type == tuple:
-        print("Tuple :", obj_type)
+        print(f"Tuple : {obj_type}")
     elif obj_type == set:
-        print("Set :", obj_type)
+        print(f"Set : {obj_type}")
     elif obj_type == dict:
-        print("Dict :", obj_type)
+        print(f"Dict : {obj_type}")
     elif obj_type == str:
-        print("String :", obj_type)
+        print(f"{obj} is in the kitchen : {obj_type}")
     else:
         print("Type not found")
 
