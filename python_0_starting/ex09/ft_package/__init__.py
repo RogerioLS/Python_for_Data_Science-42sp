1fbff0fae122a231a1bf6343f134eda01049754a
Merovingio <60794624+RogerioLS@users.noreply.github.com>
2026-05-16 21:31:47 +0000

    ✨ feat(module00): complete exercises ex06 to ex09 and document in readme

---
+++ python_0_starting/ex09/ft_package/__init__.py
@@ -0,0 +1,5 @@
+def count_in_list(lst: list, item: any) -> int:
+    """
+    Counts the occurrences of an item in a list.
+    """
+    return lst.count(item)
---
+++ python_0_starting/ex09/ft_package/src/ft_package/__init__.py
@@ -0,0 +1,3 @@
+def count_in_list(lst: list, item) -> int:
+    """Counts the occurrence of an item in a list."""
+    return lst.count(item)
